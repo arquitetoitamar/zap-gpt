b3770fe434e57d5e20a870343e01098a8328fbe8
itamar <isantos.ads@gmail.com>
2023-01-27 01:12:45 -0300

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 docker build . -t bot615026068056.dkr.ecr.us-east-1.amazonaws.com/bot:latest
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 615026068056.dkr.ecr.us-east-1.amazonaws.com
-docker tag bot:latest 615026068056.dkr.ecr.us-east-1.amazonaws.com/bot
-docker push 615026068056.dkr.ecr.us-east-1.amazonaws.com/bot
-aws ecs update-service --cluster gerenciapedidos-prd --service bot-service 
+docker tag  615026068056.dkr.ecr.us-east-1.amazonaws.com/bot:latest
+docker push 615026068056.dkr.ecr.us-east-1.amazonaws.com/bot:latest
+aws ecs update-service --cluster gerenciapedidos-prd --service bot-service  --force-new-deployment
+
 
